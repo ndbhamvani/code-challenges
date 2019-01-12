31c471ba03f1889d9da64115c64049b71d0b0523
ndbhamvani <37751257+ndbhamvani@users.noreply.github.com>
2019-01-12 10:52:07 +0000

    Minor

--- codewars/csharp/MinimizeSumOfArray.cs
+++ codewars/csharp/MinimizeSumOfArray.cs
@@ -1,13 +1,13 @@
-//Given an array of intgers , Find the minimum sum which is obtained from summing each Two integers product .
+// Given an array of intgers , Find the minimum sum which is obtained from summing each Two integers product .
 
-//Notes:
-//Array/list will contain positives only .
-//Array/list always has even size
+// Notes:
+// Array/list will contain positives only .
+// Array/list always has even size
 
-//Examples:
-//1- minSum({5,4,2,3}) ==> (5*2 + 4*3) ==> return (22)
-//2- minSum({12,6,10,26,3,24}) ==> (26*3 + 24*6 + 12*10) ==> return (342)
-//3- minSum({9,2,8,7,5,4,0,6}) ==> (9*0 + 8*2 +7*4 +6*5 = 74) ==> return (74)
+// Examples:
+// 1- minSum({5,4,2,3}) ==> (5*2 + 4*3) ==> return (22)
+// 2- minSum({12,6,10,26,3,24}) ==> (26*3 + 24*6 + 12*10) ==> return (342)
+// 3- minSum({9,2,8,7,5,4,0,6}) ==> (9*0 + 8*2 +7*4 +6*5 = 74) ==> return (74)
 
 using System;
 using System.Linq;
