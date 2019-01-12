9c42559650f27545ab14ad8dc51ed75c2f4d57ef
ndbhamvani <37751257+ndbhamvani@users.noreply.github.com>
2019-01-12 11:33:21 +0000

    Find the unique number

---
+++ codewars/python/FindTheUniqueNumber.py
@@ -0,0 +1,13 @@
+/* There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+It’s guaranteed that array contains more than 3 numbers.
+
+The tests contain some very huge arrays, so think about performance.*/
+
+def find_uniq(arr):
+   
+    arr.sort()
+    
+    return arr[0] if arr[0] != arr[2] else arr[-1]
