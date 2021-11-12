807c7305932e76114aa4381221474fb9889950aa
ndbhamvani <37751257+ndbhamvani@users.noreply.github.com>
2021-11-12 18:46:41 +0000

    Covid19App Initial Commit

---
+++ front-end/VueJS/Covid19App/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div id="app">
+  <img alt="Vue logo" src="./assets/Covid19Illustration-medium.png">
+  <Covid19Data />
+</div>
+</template>
+
+<script>
+import Covid19Data from './components/Covid19Data.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Covid19Data
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Ubuntu+Mono:wght@700&display=swap');
+
+#app {
+  font-family: 'Ubuntu Mono', monospace;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  font-size: x-large;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ front-end/VueJS/Covid19App/src/components/Covid19Data.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container">
+    <div id="reading">
+    <h3>UK Covid19 Data:</h3>
+    <figure>{{name[0].deaths}}</figure>
+    <figcaption>(Total Death Toll)</figcaption>
+    </div> 
+  </div> 
+</template>
+
+<script>
+import axios from 'axios';
+import { apiAccess } from '/User/config';
+
+  export default {
+    name: 'Covid19Data',
+    data() {
+      return {
+        name: [0],
+      };
+    },
+    created: function() {
+      axios
+        .get('https://covid-19-data.p.rapidapi.com/country/',
+          {params:{name: 'UK', date: '2020-04-01'},
+          headers: {
+            'x-rapidapi-key': apiAccess(), 
+            'x-rapidapi-host':'covid-19-data.p.rapidapi.com'
+          }
+          })
+        .then(res => {
+          this.name = res.data;
+          console.log(res.data)
+        })
+    }
+  }
+</script>
+
+<style>
+  h3 {
+    color: forestgreen;
+    padding:0%
+  }
+  #reading {
+    margin-top:5%;
+    margin-left: 35%;
+    margin-right:35%;
+    padding:0%;
+    border: green;
+    border-style: solid;
+    border-radius: 1em;
+  }
+  figure {
+    font-size: xx-large;
+  }
+  figcaption {
+    font-size: medium;
+    font-style: italic;
+  }
+</style>
